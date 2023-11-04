39cf3de9bc1bd254a0c38df411f22b548226879a
asmita-chaskar <chaskarasmita1999@email.com>
2023-11-04 12:12:44 +0530

    first commit

---
+++ credentials.py
@@ -0,0 +1,5 @@
+# User Credentials
+host = 'localhost'
+user = 'Your username here'
+password = 'Your password here'
+database = 'library_management'
---
+++ customs.py
@@ -0,0 +1,14 @@
+color_1 = "deep sky blue"
+color_2 = "gray95"
+color_3 = "black"
+color_4 = "white"
+color_5 = "red"
+color_6 = "green3"
+color_7 = "yellow"
+font_1 = "times new roman"
+font_2 = "helvetica"
+
+columns = ('book_id', 'book_name', 'author', 'edition', 'price', 'qty')
+
+columns_1 = ('book_id', 'book_name', 'student_roll', 'student_name', 'course', 'subject', 'issue_date', 'return_date')
+ 
